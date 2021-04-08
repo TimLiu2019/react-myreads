8fafc6ebe5f31fb562c54c1602b1f82d96918bee
TimLiu2019 <qitingliu7@gmail.com>
2021-04-08 10:18:46 -0400

    update link button for components

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -13,7 +13,6 @@ import { Link } from 'react-router-dom'
             to='/'>
               Close
           </Link>
-        <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
         <div className="search-books-input-wrapper">
           {/*
             NOTES: The search from BooksAPI is limited to a particular set of search terms.
