bee03c0f56abbad95e8944bb851fba30c4e26c1a
TimLiu2019 <qitingliu7@gmail.com>
2021-04-09 13:19:04 -0400

    list books using api

--- src/ShelfBook.js
+++ src/ShelfBook.js
@@ -2,19 +2,20 @@ import React from "react";
 //import PropTypes from 'prop-types'
 
 const ShelfBook = props => {
+   // const{book} = props;
+   const style = {
+    width: 128,
+    height: 193,
+    backgroundImage: `url(${props.book['imageLinks']['thumbnail']})` 
+  }
     
-  //let numberOfGames = '/*';
+
   return (
     <div className="book">
     <div className="book-top">
       <div
         className="book-cover"
-        style={{
-          width: 128,
-          height: 193,
-          backgroundImage:
-            'url("http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api")'
-        }}
+        style={style}
       ></div>
       <div className="book-shelf-changer">
         <select>
@@ -30,8 +31,8 @@ const ShelfBook = props => {
         </select>
       </div>
     </div>
-    <div className="book-title">1776</div>
-    <div className="book-authors">David McCullough</div>
+    <div className="book-title">{props.book.title}</div>
+    <div className="book-authors">{props.book.authors[0]}</div>
   </div>
   );
 
